6251e58782a88186f86fd111ed7cfb4bf3642ab0
diubaldo94 <diubaldo94@gmail.com>
2021-10-23 18:11:18 +0200

    first commt

---
+++ WeatherApiTechTask/WeatherApiTechTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherApiTechTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
