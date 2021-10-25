4086eeaacab9733dd0fd2a265cf67a0aaf24101c
diubaldo94 <diubaldo94@gmail.com>
2021-10-25 12:52:06 +0200

    wrtten basic ue case test skeleton

--- WeatherApiTechTask.Test/WeatherApiTechTask.Test/UnitTest1.cs
+++ WeatherApiTechTask.Test/WeatherApiTechTask.Test/UnitTest1.cs
@@ -5,42 +5,58 @@ namespace WeatherApiTechTask.Test
 {
     public class Tests
     {
-        private IWeatherApp _app;
+        private readonly IWeatherApp _sut;
         //private IWeatherLoader _loader;
         //private IPublisher _publisher;
         //private IRestClient _restClient;
+        private readonly Mock<INotifier> _consoleMock = new Mock<INotifier>();
+        private readonly Mock<IRestClient> _restClientMockForCity = new Mock<IRestClient>();
+        private readonly Mock<IRestClient> _restClientMockForWeather = new Mock<IRestClient>();
 
+        private readonly CityLoader.Configuration _cityConfig = new CityLoader.Configuration();
+        private readonly WeatherLoader.Configuration _weatherConfig = new WeatherLoader.Configuration();
+
+
+        //PROVA NETMOCK
         [SetUp]
         public void Setup()
-        {
-            var consoleMock = new Mock<INotifier>();
-            var restClientMockForCity = new Mock<IRestClient>();
-            var restClientMockForWeather = new Mock<IRestClient>();
-
-            _app = new WeatherApp(
+        {          
+            _sut = new WeatherApp(
                 new Loader(
-                    new CityLoader(restClientMockForCity.Object),
-                    new WeatherLoader(restClientMockForWeather.Object)
+                    new CityLoader(_restClientMockForCity.Object, cityConfig),
+                    new WeatherLoader(_restClientMockForWeather.Object, weatherConfig)
                     ),
-                new Publisher(consoleMock.Object)
+                new Publisher(_consoleMock.Object)
                 );
         }
 
-        [Test]
-        public void Test1()
+        public void LoadSomeCitiesWithWeatherAndNotifyThem()
         {
-//            1.GET ALL CITIES FROM https://api.musement.com/api/v3/cities
+            _restClientMockForCity.Setup(c => c.Get<CityInfoResponse>(_cityConfig.Url, new { }))
+                .Returns(new CityInfoResponse()).Verifiable();
+            _restClientMockForWeather.Setup(c => c.Get<WeatherInfoResponse>(_weatherConfig.Url, new { }))
+                .Returns(new WeatherInfoResponse()).Verifiable();
 
-//            2.foreach city get
-//http://api.weatherapi.com/v1/forecast.json?key=%5Byour-key%5D&q=%5Blat%5D,%5Blong%5D&days=2
-//            (days meglio renderli parametrici)
-//            API KEY DA FREE PLAN
-//            LAT Long potrebbero non essere precisi non fa niente
+            _consoleMock.Verify(c => c.Notify(new WeatherOutcome(new { "", "" })), Times.Once);
 
-//            3.Processare i dati ricevuti(farlo in maniera scalabile):
-//                Console.writeline
+            _sut.Run();
 
-
+            _restClientMockForCity.Verify();
+            _restClientMockForWeather.Verify();
         }
+
+
+
+        //            1.GET ALL CITIES FROM https://api.musement.com/api/v3/cities
+
+
+        //            2.foreach city get
+        //http://api.weatherapi.com/v1/forecast.json?key=%5Byour-key%5D&q=%5Blat%5D,%5Blong%5D&days=2
+        //            (days meglio renderli parametrici)
+        //            API KEY DA FREE PLAN
+        //            LAT Long potrebbero non essere precisi non fa niente
+
+        //            3.Processare i dati ricevuti(farlo in maniera scalabile):
+        //                Console.writeline
     }
 }
